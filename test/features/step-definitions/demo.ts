d990df0cb5f41a80bf352072048347e9646dcaf1
Combatd <combatdstar@gmail.com>
2023-01-16 17:43:31 -0800

    Search WDIO on google

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -1,6 +1,20 @@
 import { Given, When, Then } from "@wdio/cucumber-framework";
 
-Given(/^Google page is opened$/, function() {
-    browser.url('https://www.google.com');
-    browser.pause(70000);
-})
+Given(/^Google page is opened$/, async function() {
+  console.log('Before opening browser...');
+  await browser.url('https://www.google.com');
+  await browser.pause(1000);
+  console.log('After opening browser...');
+});
+
+When(/^Search with (.*)$/, async function(searchItem) {
+  console.log(`>> searchItem ${searchItem}`);
+  let ele = await $(`[name=q]`);
+  await ele.setValue(searchItem);
+  await browser.keys('Enter');
+});
+
+Then(/^Click on the first search result$/, async function() {
+  let ele = await $('<h3>');
+  ele.click();
+});
