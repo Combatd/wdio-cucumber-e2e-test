704190b7a75fb861feaf88fca5c77576deb93ab9
Combatd <combatdstar@gmail.com>
2023-02-17 12:38:34 -0800

    Refresh page to retry login based on trycatch

--- test/features/step-definitions/given.ts
+++ test/features/step-definitions/given.ts
@@ -13,19 +13,30 @@ Given(/^Login to inventory web app$/, async function() {
   /**
    * 2. Login to inventory app
    */
-  await $('#user-name').setValue('standard_user');
-  await $('#password').setValue('secret_sauce'); // in actual projects, use environment variables
-  await $('#login-button').click();
+  try {
+    await $('#user-name').setValue('standard_user');
+    await $('#password').setValue('secret_sauce'); // in actual projects, use environment variables
+    await $('#login-button').click();
+  } catch (err) {
+    console.log('Error in first login, Retrying...');
+    await browser.refresh();
+    await browser.pause(2000);
+    await $('#user-name').setValue('standard_user');
+    await $('#password').setValue('secret_sauce'); // in actual projects, use environment variables
+    await $('#login-button').click();
+  }
 
   /**
    * Login with another user
    */
-  await browser.pause(2000);
-  await browser.reloadSession();
-  await browser.url('https://saucedemo.com');
-  await $('#user-name').setValue('problem_user');
-  await $('#password').setValue('secret_sauce'); // in actual projects, use environment variables
-  await $('#login-button').click();
+  // await browser.pause(2000);
+  // await browser.reloadSession();
+  // await browser.url('https://saucedemo.com');
+  // await $('#user-name').setValue('problem_user');
+  // await $('#password').setValue('secret_sauce'); // in actual projects, use environment variables
+  // await $('#login-button').click();
+
+
 
   await browser.debug();
 });
