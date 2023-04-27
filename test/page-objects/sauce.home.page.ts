cd21e68f5c75122e18e372a478172ed3a55b8d0e
Combatd <combatdstar@gmail.com>
2023-04-27 14:20:39 -0700

    Create page objects actions

--- test/page-objects/sauce.home.page.ts
+++ test/page-objects/sauce.home.page.ts
@@ -13,6 +13,55 @@ class HomePage extends Page {
   get loginInputBox() { return $('#login-button') }
 
   /**Page Actions */
+  enterUsername = async function(testid: string, username: string) {
+    if (!username) throw Error(`Given username: ${username} is not valid`);
+
+    try {
+      username = username.trim();
+      await this.typeInto(await this.usernameInputBox, username);
+      reporter.addStep(testid, 'info', `${username} entered sccessfuly`);
+    } catch (err) {
+      err.message = `Error entering username: ${username}, ${err.message}`;
+      throw err;
+    }
+
+  }
+
+  enterPassword = async function(testid: string, password: string) {
+    if (!password) throw Error(`Given username is not valid`);
+
+    try {
+      password = password.trim();
+      await this.typeInto(await this.usernameInputBox, password);
+      reporter.addStep(testid, 'info', `password entered sccessfuly`);
+    } catch (err) {
+      err.message = `Error entering password, ${err.message}`;
+      throw err;
+    }
+
+  }
+
+  clickLoginBtn = async function(testid: string) {
+    try {
+      await this.click(await this.loginBtn);
+      reporter.addStep(testid, 'info', `login button clicked`);
+    } catch (err) {
+      err.message = `Error clicking login button, ${err.message}`;
+      throw err;
+    }
+
+  }
+
+  loginToSauceApp = async function(testid: string, username: string, password: string) {
+    try {
+      await this.enterUsername(testid, username);
+      await this.enterPassword(testid, password);
+      await this.clickLoginBtn(testid);
+    } catch (err) {
+      throw err;
+    }
+  }
+
 
 }
 
