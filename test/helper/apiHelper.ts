636389411ce4fc98bb567e5e8d94d41934081f84
Combatd <combatdstar@gmail.com>
2023-04-29 17:13:50 -0700

    Log request object from supertest package

---
+++ test/helper/apiHelper.ts
@@ -0,0 +1,5 @@
+import request from 'supertest';
+
+// What is request
+console.log(`The typeof reqeust: ${typeof request}`); // => function
+console.log(`Number of args: ${request.length}`); // 1 arg
